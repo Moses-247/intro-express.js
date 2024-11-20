586d432a054e533b5029ce05e50289b98277f23a
Moses <asugam9@gmail.com>
2024-11-20 21:17:19 +0300

    Get req, res

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+//Setting up Express
 import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>{
+app.get("/", (request, response) => {
+    response.send("Hello World")
+});
+
+app.get("/api/users", (req, res) => {
+    res.send([
+        {id: 1, username: "Moses Asuga",displayName: "Moses"},
+        {id: 2, username: "Rayan Hassan",displayName: "Rayan"},
+        {id: 3, username: "Grace Boke", displayName: "Grace"}
+    ]);
+});
+
+app.get('/api/products', (req, res) =>{
+    res.send([{}])
+})
+
+app.listen(3000, () =>{
     console.log("server running at localhost:3000");
 });
